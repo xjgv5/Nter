e1f3438de4958dc26f25adbf7119e5a3c3835f53
Javier Gomez <javier.gov@outlook.es>
2025-11-18 17:15:59 -0600

    Rutas y layout creado

---
+++ src/components/Corte.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Corte() {
+    return (
+        <div>
+            <h1>Corte</h1>
+        </div>
+    );
+}
+
+export default Corte;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <h1>Footer</h1>
+        </footer>
+    );
+}
+
+export default Footer;
